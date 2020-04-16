c4e8ddba20ac5c894d6a4cc1d239f74bf9a3585e
Ivaylo Brezoev <srce@mail.bg>
2020-04-16 14:02:16 +0300

    FIXED!!! HTML files are rearranged, delete dialog window is reworked.

--- app/directives/deleteDialogDisplay.js
+++ app/directives/deleteDialogDisplay.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 genApp.directive('deleteDialog', ['$rootScope', 'eventsConstant', function ($rootScope, eventsConstant) {
     return {
         restrict: 'A',
@@ -28,30 +27,6 @@ genApp.directive('deleteDialog', ['$rootScope', 'eventsConstant', function ($roo
                 }
 
                 scope.isShown = false;
-=======
-genApp.directive('deleteDialog', ['$route', 'genActionsService', function ($route, genActionsService) {
-    return {
-        restrict: 'A',
-        templateUrl: '../views/directives/deleteDialogView.html',
-        scope: {
-            info: '=',
-            dialog: '='
-        },
-        link: function (scope) {
-            scope.confirmHandler = function () {
-                if ('generator' in scope.info) {
-                    genActionsService.deleteNumber(scope.info.idx, scope.info.generator);
-                } else {
-                    genActionsService.deleteGenerator(scope.info.idx);
-                }
-                $route.reload();
-            };
-
-            scope.cancelHandler = function (event) {
-                if (event.target.id === 'cancel') {
-                    $route.reload();
-                }
->>>>>>> 92dd1ff7143e25a6d0ab05e9ed603c68d5f252a4
             };
         }
     };
